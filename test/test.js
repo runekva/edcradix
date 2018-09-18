5ee8361318f8f9c04548bdd6b7d5e180cd262460
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2018-09-18 10:41:50 +0200

    Adding first tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert')
+var expect = require('chai').expect
+
+var normalizePort = require('../src/index.js')
+
+describe('normalizePort(val)', function () {
+  it('should normalise sting 3000 to int 3000', function () {
+    var port = '3000'
+    var normalizedPort = normalizePort(port)
+
+    expect(normalizedPort).to.be.equal(3000)
+  })
+
+  it('should return value of type number', function () {
+    var port = '3000'
+    var normalizedPort = normalizePort(port)
+
+    expect(normalizedPort).to.be.a('number')
+  })
+})
