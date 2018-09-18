6a438e3d2822a43c0030ccb5ddf4351224adda70
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2018-09-18 09:51:26 +0200

    Getting quote from MaShape

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+var express = require('express');
+var request = require('request');
+
+var app = express();
+var apiKey = process.env.QUOTEKEY;
+
+var quoteUrlOptions = {
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'X-Mashape-Key': apiKey
+    },
+    form: {
+        cat: 'famous',
+        count: 1
+    }
+};
+
+request.post(quoteUrlOptions, function (error, response, body) {
+    if (error) {
+        console.log('Error getting quote : ', error);
+    } else {
+        console.log('Got response: ' + body)
+    }
+});
